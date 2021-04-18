b74edb4e54ca4ef8a4f12f93c3a9a4f0cf36357b
Artem Sychov <[artem.suchov@gmail.com]>
2021-04-18 15:11:58 +0300

    exercism python track some challanges with tests

---
+++ pangram/pangram.py
@@ -0,0 +1,17 @@
+def is_pangram(sentence):
+    '''every letter in a sentence should be [a-z] at least onece otherwhise return False'''
+    c = 0
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    phraseLetters = ""
+    for char in sentence.lower():
+        for letter in alphabet:
+            if char == letter and char not in phraseLetters:
+                phraseLetters += char
+    for char in phraseLetters:
+        for letter in alphabet:
+            if char == letter:
+                c += 1
+    if c == 26:
+        return True
+    else:
+        return False
