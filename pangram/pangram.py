2aa1196446e9eb3291282dc6aa87c1bd8e53f70e
Artem Sychov <[artem.suchov@gmail.com]>
2021-04-19 22:28:06 +0300

    common modern solution with unilization of generator

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,12 +1,8 @@
 import string 
 
-def is_pangram(sentence):
+def is_pangram(sentence: str) -> bool:
     '''Every letter in a sentence should be [a-z] at least onece otherwhise return False.'''
     sentence = sentence.lower()
 
     # finding letter by letter in the word and return fase if any missing     
-    for c in string.ascii_lowercase:
-        if c not in sentence: 
-            return False
-    return True
-
+    return all(letter in sentence for letter in string.ascii_lowercase)
