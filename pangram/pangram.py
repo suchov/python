56dc075e970153fe4b5dfb5622266cbbb55f26f4
Artem Sychov <[artem.suchov@gmail.com]>
2021-04-19 22:00:03 +0300

    last commit for now - problem solved

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,7 +1,12 @@
+import string 
+
 def is_pangram(sentence):
     '''Every letter in a sentence should be [a-z] at least onece otherwhise return False.'''
-    alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
-    # finding letter by letter in the word and return fase if any missing
-    for c in alphabet:
-        if c not in sentence.lower(): return False
+    sentence = sentence.lower()
+
+    # finding letter by letter in the word and return fase if any missing     
+    for c in string.ascii_lowercase:
+        if c not in sentence: 
+            return False
     return True
+
